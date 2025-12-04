42a756631289e1fb9f23062793a9037026d4c25f
Tanner Knapp <shadow@shadowthewuff.gay>
2025-12-04 13:04:35 -0600

    g

--- phantom.js
+++
@@ -1,7 +0,0 @@
-// With PNPM, this should fail because we didn't install mime-types
-try {
-  const mime = require('mime-types');
-  console.log('Successfully loaded mime-types:', mime.lookup('json'));
-} catch (e) {
-  console.error('Failed to load mime-types:', e.message);
-}
